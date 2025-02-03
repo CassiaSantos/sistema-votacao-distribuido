45a8b50bedc68b32b48e548c2dbcf817582beeff
correaisaac <correaisaac25@gmail.com>
2025-02-03 17:30:54 -0300

    refactor: deleta arquivos antigos não mais necessários

--- src/config/dbConect.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-//conexão com o mongoDB:
-async function conectaNoBancoDeDados() {
-    mongoose.connect(process.env.DB_CONNECTION_STRING);
-    return mongoose.connection;
-}
-
-//exportando a função para ser usada em outros arquivos:
-export default conectaNoBancoDeDados;
