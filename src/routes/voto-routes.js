fc1f1139c23c92ef1f379f89ca66f4d942a83556
Cássia Oliveira dos Santos <cassia.oliveira.profissional@gmail.com>
2025-02-16 17:54:35 -0300

    refactor: deleta arquivos de rotas do sistema antigo

--- src/routes/voto-routes.js
+++
@@ -1,9 +0,0 @@
-// src/routes/voto-routes.js
-const express = require('express');
-const router = express.Router();
-const VotoController = require('../controllers/voto-controller');
-const authMiddleware = require('../middlewares/auth-middleware');
-
-router.post('/', authMiddleware, VotoController.create);
-
-module.exports = router;
