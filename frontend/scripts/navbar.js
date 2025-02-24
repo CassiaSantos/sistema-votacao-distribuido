8afbcc9293099489b09b18fd016c33eae90e0947
correaisaac <correaisaac25@gmail.com>
2025-02-24 17:43:51 -0300

    refactor: adiciona cabeçalho e footer na página votacoes_crud.html

--- frontend/scripts/navbar.js
+++ frontend/scripts/navbar.js
@@ -1,7 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
     const navbar = `
-        <nav class="navbar navbar-expand-lg" style="background: var(--primary-color);">
+        <nav class="navbar navbar-expand-lg" style="background: var(--primary-color); height:6rem;">
             <div class="container">
+                <img style="max-width: 2rem; margin: 1rem;"
+                src="https://github.com/CassiaSantos/sistema-votacao-distribuido/blob/main/frontend/img/voto_computer.png?raw=true"  alt="Logo" class="logo"> <!--Imagem vinda de https://cdn-icons-png.flaticon.com/-->
                 <a class="navbar-brand text-white fw-bold" href="#">DecideAí</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
                     <span class="navbar-toggler-icon"></span>
@@ -9,13 +11,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav ms-auto">
                         <li class="nav-item">
-                            <a class="nav-link text-white" href="index.html">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-white" href="votacoes.html">Votações</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-white" href="admin_login.html">Admin</a>
+                            <a class="nav-link text-white" href="../../index.html">Home</a>
                         </li>
                     </ul>
                 </div>
